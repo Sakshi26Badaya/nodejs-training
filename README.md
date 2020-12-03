27a21b4acf984893b1cd13cf2643c9c041e86f14
Sakshi26Badaya <badaya.sakshi03@gmail.com>
2020-12-03 17:22:12 +0530

    files added  commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# nodejs-training
+#prerequisists:
+node 8
+
+#how to setup 
+1) npm install
+
+2) create .env file from .env.example and update configuration 
+
+#run app 
+npm run watch
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+const PORT: number = Number(process.env.PORT);
 const app: express.Application = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log(`app listening on port  ${PORT} `);
 });
 
